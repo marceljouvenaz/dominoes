7787fbb80f46b80bc85e5e2eaf81527e1b38f372
marceljouvenaz <mjouvenaz@gmail.com>
2021-06-05 18:57:21 +0200

    basic idea of the game

--- game.php
+++ game.php
@@ -4,11 +4,48 @@
 class game
 {
 /**
- * - deal tiles
- * - alternate players
- * - choose tile to play
- *   - alternate action
- * - alter map
- * - end game
+ *
+ * pseudo code the steps of the game
+ *
 */
+
+    const MAXPLAYER = 2;
+    const MAXTILES = 7;
+    private $playOn;
+
+    public function startGame() {
+        $this->playOn = true;
+        $this->deal();
+        $this->selectStartTile();
+        $this->playGame();
+    }
+
+    public function deal(){
+        for ($player = 0; $player < $this::MAXPLAYER; $player++){
+            for ($card = 0; $card < $this::MAXTILES; $card++){
+                $this->pickRandomTile($player);
+            }
+        }
+
+    }
+    private function selectStartTile(){
+        $this->pickRandomTile("table");
+    }
+    private function playGame(){
+        $player = 0;
+
+        while ($this->playOn){
+            // player tries to play a tile, draws a tile if fails to play one.
+            $this->playTile($player);
+            $this->nextPlayer($player);
+            $this->checkPlayOn($player);
+            $this->generateOutput();
+        }
+    }
+    private function pickRandomTile($p) {
+        // add a random tile to player $p
+        // probably easiest to consider all 28 tiles as an array with value and holder of the tile.
+        // i.e. tiles[$i] = [ lowest face, highest face, player (or stack, or table)]
+    }
+
 }
